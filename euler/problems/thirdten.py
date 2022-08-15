ef8ad40a74f57d9012badd886776738f898ed003
Auren Clarke <aurenclarke@gmail.com>
2022-08-15 18:25:36 +1200

    started 23

--- euler/problems/thirdten.py
+++ euler/problems/thirdten.py
@@ -58,6 +58,44 @@ def problem_twenty_two(datafile: str) -> int:
     return output
 
 
+def problem_twenty_three(num: int) -> int:
+    """
+    A perfect number is a number for which the sum of its proper divisors is exactly equal to the number.
+    For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28,
+    which means that 28 is a perfect number.
+    A number n is called deficient if the sum of its proper divisors is less than n and it is called
+    abundant if this sum exceeds n.
+    As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be
+    written as the sum of two abundant numbers is 24.
+
+    By mathematical analysis, it can be shown that all integers greater than 28123 can be written
+    as the sum of two abundant numbers.
+
+    However, this upper limit cannot be reduced any further by analysis even though it is known that
+    the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+    Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+    """
+    output = 0
+    for i in range(1, num):
+        perfect = False
+        deficient = False
+        abundant = False
+        total = sum(prime.divisors(i, True))
+
+        if total == i:
+            perfect = True
+        elif total < i:
+            deficient = True
+        else:
+            abundant = True
+
+        if abundant:
+            print(f"Number: {i}, Sum: {total}")
+
+    return output
+
+
 if __name__ == "__main__":
-    test = problem_twenty_two("p022_names.txt")
+    test = problem_twenty_three(100)
     print(test)
