f427febc864d169839a0dfc6e42d32fc656612eb
Auren Clarke <auren.clarke1@stats.govt.nz>
2022-08-15 15:42:00 +1200

    completed 22

--- euler/problems/thirdten.py
+++ euler/problems/thirdten.py
@@ -1,6 +1,8 @@
 """Project Euler: Problems 21-30"""
 import math
 import numpy as np
+import csv
+import string
 
 from euler import prime
 
@@ -28,6 +30,34 @@ def problem_twenty_one(num):
     return output
 
 
+def problem_twenty_two(datafile: str) -> int:
+    """
+    Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names,
+    begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this
+    value by its alphabetical position in the list to obtain a name score.
+    For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53,
+    is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+    What is the total of all the name scores in the file?
+    """
+    output = 0
+
+    with open(f"./data/{datafile}", "r") as file:
+        input = file.read()
+
+    names = input.replace("\n", " ").replace('"', "").split(",")
+    names.sort()
+
+    letters = list(string.ascii_uppercase)
+
+    for i in range(0, len(names)):
+        word_value = 0
+        for y in names[i]:
+            word_value += letters.index(y) + 1
+        output += word_value * (i + 1)
+
+    return output
+
+
 if __name__ == "__main__":
-    test = problem_twenty_one(1000)
+    test = problem_twenty_two("p022_names.txt")
     print(test)
--- tests/test_thirdten.py
+++ tests/test_thirdten.py
@@ -11,3 +11,10 @@ def test_problem_twenty_one():
     actual = thirdten.problem_twenty_one(1000)
     expected = 504
     assert actual == expected
+
+
+def test_problem_twenty_two():
+    """testing problem_twenty_two"""
+    actual = thirdten.problem_twenty_two("p022_names.txt")
+    expected = 871198282
+    assert actual == expected
