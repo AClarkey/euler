59b81f2c66b888f40b864ea692a5174d4cfa9779
Auren Clarke <aurenclarke@gmail.com>
2022-09-23 19:41:39 +1200

    Still on 61

--- euler/problems/problems61to70.py
+++ euler/problems/problems61to70.py
@@ -1,8 +1,10 @@
 """Project Euler: Problems 61 to 70"""
+from re import I
 import time
 import string
 import math
 import itertools
+import copy
 
 import statistics
 from statistics import mode
@@ -13,6 +15,78 @@ from euler import helper, prime
 
 
 def problem_61(number: int) -> int:
+    """
+    Find the sum of the only ordered set of six cyclic 4-digit numbers for
+    which each polygonal type: triangle, square, pentagonal, hexagonal,
+    heptagonal, and octagonal, is represented by a different number in the set.
+    """
+
+    def is_type(number: int, types: list) -> list:
+        if helper.is_triangle_number(number):
+            types[0] += 1
+        if helper.is_square_number(number):
+            types[1] += 1
+        if helper.is_pentagonal_number(number):
+            types[2] += 1
+        if helper.is_hexagonal_number(number):
+            types[3] += 1
+        if helper.is_heptagonal_number(number):
+            types[4] += 1
+        if helper.is_octagonal_number(number):
+            types[5] += 1
+        return types
+
+    numbers = []
+    for i in range(1000, 10000):
+        if helper.is_triangle_number(i):
+            numbers.append(i)
+        if helper.is_square_number(i):
+            numbers.append(i)
+        if helper.is_pentagonal_number(i):
+            numbers.append(i)
+        if helper.is_hexagonal_number(i):
+            numbers.append(i)
+        if helper.is_heptagonal_number(i):
+            numbers.append(i)
+        if helper.is_octagonal_number(i):
+            numbers.append(i)
+
+    for i in numbers:
+        if str(numbers)[2:3] == "0":
+            numbers.remove(i)
+
+    for a in numbers:
+        print("break")
+        digits = copy.copy(numbers)
+        output = [a]
+        digits.remove(a)
+
+        for b in digits:
+            if str(a)[2:4] == str(b)[0:2]:
+                output.append(b)
+                digits.remove(b)
+                for c in digits:
+                    if str(b)[2:4] == str(c)[0:2]:
+                        output.append(c)
+                        digits.remove(c)
+                        for d in digits:
+                            if str(c)[2:4] == str(d)[0:2]:
+                                output.append(d)
+                                digits.remove(d)
+                                for e in digits:
+                                    if str(d)[2:4] == str(e)[0:2]:
+                                        output.append(e)
+                                        digits.remove(e)
+                                        output.append(int(str(e)[2:4] + str(a)[0:2]))
+                                        if (
+                                            len(output) == 6
+                                            and len(str(output[-1])) == 4
+                                        ):
+                                            for i in output:
+                                                print(i, is_type(i, [0, 0, 0, 0, 0, 0]))
+
+
+def problem_61b(number: int) -> int:
     """
     Find the sum of the only ordered set of six cyclic 4-digit numbers for
     which each polygonal type: triangle, square, pentagonal, hexagonal,
@@ -60,16 +134,15 @@ def problem_61(number: int) -> int:
 
                                             five_end = str(five)[2:4]
                                             six = five_end + str(one)[0:2]
-                                            if is_type(int(six), types).count(1) == 6:
-                                                print(
-                                                    one,
-                                                    two,
-                                                    three,
-                                                    four,
-                                                    five,
-                                                    six,
-                                                    is_type(int(six), types),
-                                                )
+                                            print(
+                                                one,
+                                                two,
+                                                three,
+                                                four,
+                                                five,
+                                                six,
+                                                is_type(int(six), types),
+                                            )
 
 
 if __name__ == "__main__":
