88a95e192de525708e7553c4e1986f6f0dc93e6e
Auren Clarke <auren.clarke1@stats.govt.nz>
2022-09-28 15:13:46 +1300

    still working on 61 :), breaking it out and making super explicit. Looks promising

--- euler/problems/problems61to70.py
+++ euler/problems/problems61to70.py
@@ -21,130 +21,59 @@ def problem_61(number: int) -> int:
     heptagonal, and octagonal, is represented by a different number in the set.
     """
 
-    def is_type(number: int, types: list) -> list:
-        if helper.is_triangle_number(number):
-            types[0] += 1
-        if helper.is_square_number(number):
-            types[1] += 1
-        if helper.is_pentagonal_number(number):
-            types[2] += 1
-        if helper.is_hexagonal_number(number):
-            types[3] += 1
-        if helper.is_heptagonal_number(number):
-            types[4] += 1
-        if helper.is_octagonal_number(number):
-            types[5] += 1
-        return types
-
-    numbers = []
+    numbers = set()
     for i in range(1000, 10000):
         if helper.is_triangle_number(i):
-            numbers.append(i)
+            numbers.add(i)
         if helper.is_square_number(i):
-            numbers.append(i)
+            numbers.add(i)
         if helper.is_pentagonal_number(i):
-            numbers.append(i)
+            numbers.add(i)
         if helper.is_hexagonal_number(i):
-            numbers.append(i)
+            numbers.add(i)
         if helper.is_heptagonal_number(i):
-            numbers.append(i)
+            numbers.add(i)
         if helper.is_octagonal_number(i):
-            numbers.append(i)
+            numbers.add(i)
 
-    print(len(numbers))
+    # print(len(numbers), sorted(numbers))
+    output = []
 
-    # for i in numbers:
-    #     if str(numbers)[2:3] == "0":
-    #         numbers.remove(i)
+    for i in numbers:
+        for y in numbers:
+            if str(i)[len(str(i)) - 2 :] == str(y)[0:2]:
+                output.append(int(str(i) + str(y)))
+    output2 = []
+    for i in output:
+        for y in numbers:
+            if str(i)[len(str(i)) - 2 :] == str(y)[0:2]:
+                output2.append(int(str(i) + str(y)))
+    output3 = []
+    for i in output2:
+        for y in numbers:
+            if str(i)[len(str(i)) - 2 :] == str(y)[0:2]:
+                output3.append(int(str(i) + str(y)))
+    output4 = []
+    for i in output3:
+        for y in numbers:
+            if str(i)[len(str(i)) - 2 :] == str(y)[0:2]:
+                output4.append(int(str(i) + str(y)))
+    output = []
+    for i in output4:
+        last_digit = int(str(i)[len(str(i)) - 2 :] + str(i)[0:2])
+        if last_digit in numbers:
+            output.append(int(str(i) + str(last_digit)))
 
-    # for a in numbers:
-    #     print("break")
-    #     digits = copy.copy(numbers)
-    #     output = [a]
-    #     digits.remove(a)
+    for var in output:
+        numbers = []
+        for i in range(0, 24, 4):
+            numbers.append(str(var)[i : i + 4])
+        if len(list(set(numbers))) == 6:
+            print(numbers)
+        else:
+            output.remove(var)
 
-    #     for b in digits:
-    #         if str(a)[2:4] == str(b)[0:2]:
-    #             output.append(b)
-    #             digits.remove(b)
-    #             for c in digits:
-    #                 if str(b)[2:4] == str(c)[0:2]:
-    #                     output.append(c)
-    #                     digits.remove(c)
-    #                     for d in digits:
-    #                         if str(c)[2:4] == str(d)[0:2]:
-    #                             output.append(d)
-    #                             digits.remove(d)
-    #                             for e in digits:
-    #                                 if str(d)[2:4] == str(e)[0:2]:
-    #                                     output.append(e)
-    #                                     digits.remove(e)
-    #                                     output.append(int(str(e)[2:4] + str(a)[0:2]))
-    #                                     if (
-    #                                         len(output) == 6
-    #                                         and len(str(output[-1])) == 4
-    #                                     ):
-    #                                         for i in output:
-    #                                             print(i, is_type(i, [0, 0, 0, 0, 0, 0]))
-
-
-def problem_61b(number: int) -> int:
-    """
-    Find the sum of the only ordered set of six cyclic 4-digit numbers for
-    which each polygonal type: triangle, square, pentagonal, hexagonal,
-    heptagonal, and octagonal, is represented by a different number in the set.
-    """
-
-    def is_type(number: int, types: list) -> list:
-        if helper.is_triangle_number(number):
-            types[0] += 1
-        if helper.is_square_number(number):
-            types[1] += 1
-        if helper.is_pentagonal_number(number):
-            types[2] += 1
-        if helper.is_hexagonal_number(number):
-            types[3] += 1
-        if helper.is_heptagonal_number(number):
-            types[4] += 1
-        if helper.is_octagonal_number(number):
-            types[5] += 1
-        return types
-
-    for one in range(1000, 10000):
-        types = [0, 0, 0, 0, 0, 0]
-        if is_type(one, types).count(1) == 1:
-
-            one_end = str(one)[2:4]
-            for b in range(10, 100):
-                two = one_end + str(b)
-                if is_type(int(two), types).count(1) == 2:
-
-                    two_end = str(two)[2:4]
-                    for c in range(10, 100):
-                        three = two_end + str(c)
-                        if is_type(int(three), types).count(1) == 3:
-
-                            three_end = str(three)[2:4]
-                            for d in range(10, 100):
-                                four = three_end + str(d)
-                                if is_type(int(four), types).count(1) == 4:
-
-                                    four_end = str(four)[2:4]
-                                    for e in range(10, 100):
-                                        five = four_end + str(e)
-                                        if is_type(int(five), types).count(1) == 5:
-
-                                            five_end = str(five)[2:4]
-                                            six = five_end + str(one)[0:2]
-                                            print(
-                                                one,
-                                                two,
-                                                three,
-                                                four,
-                                                five,
-                                                six,
-                                                is_type(int(six), types),
-                                            )
+        for y in numbers: 
 
 
 if __name__ == "__main__":
